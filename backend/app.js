8e415f3261866767b3b2479c18b9a2872c596729
Shayan Khaksar <sh.khaksar95@gmail.com>
2021-07-19 16:23:59 +0200

    Added Index router for health check

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+app.use('/', indexRouter);
 app.use('/api/', indexRouter);
 app.use('/api/users/', usersRouter);
 
